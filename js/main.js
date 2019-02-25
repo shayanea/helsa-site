33aa32074d965274feace06bd24e4f71ed8c4c5f
Shayan Araghi <shayanaraghi@live.com>
2019-02-25 12:39:39 +0330

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(window).load(function() {
+  $(".banner .owl-carousel").owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+});
