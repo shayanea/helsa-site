7f9eb39b8980724aafea752593c4cae9981def6a
Shayan Araghi <shayanaraghi@live.com>
2019-07-20 03:49:32 +0430

    implement laning page

--- js/main.js
+++ js/main.js
@@ -4,9 +4,179 @@ $(window).load(function() {
       String(t).toLowerCase()
     );
   }
+
   $(document).on("click", ".resource-container .resource-title", function(e) {
     $(".resource-container").toggleClass("expand");
   });
+
+  $(document).on("click", "#goToFeatures", function(e) {
+    e.preventDefault();
+    $("html,body").animate({
+      scrollTop: $(".section-1").offset().top
+    });
+  });
+
+  $(document).on("click", "#goToDownload", function(e) {
+    e.preventDefault();
+    $("html,body").animate({
+      scrollTop: $(".section-5").offset().top
+    });
+  });
+
+  $(".goal").on("input", function() {
+    var goal = $(".goal").val(),
+      text = "";
+    if (Number(goal) === 2) {
+      text = "کم کردن";
+    }
+    if (Number(goal) === 3) {
+      text = "اضافه کردن";
+    }
+    console.log(Number(goal));
+    $("#rangePicker").css("display", Number(goal) === 2 || Number(goal) === 3 ? "block" : "none");
+    $("#range-title").html(text);
+  });
+
+  $(".range[type=range]").on("input", function() {
+    $("#range-result").html($(this).val() + " کیلو‌گرم در هفته");
+  });
+
+  function toEnglishDigits(string) {
+    string = typeof string === "number" ? JSON.stringify(string) : string;
+    var e = "۰".charCodeAt(0);
+    string = string.replace(/[۰-۹]/g, function(t) {
+      return t.charCodeAt(0) - e;
+    });
+    e = "٠".charCodeAt(0);
+    string = string.replace(/[٠-٩]/g, function(t) {
+      return t.charCodeAt(0) - e;
+    });
+    return string;
+  }
+
+  $(document).on("click", ".bmi-calculator .submit-btn", function(e) {
+    e.preventDefault();
+    var formData = $(".bmi-calculator").serializeArray(),
+      isValid = true;
+    $(".bmi-calculator input, .bmi-calculator select").each(function() {
+      $(this).val(0);
+      if ($.trim($(this).val()) == "") {
+        isValid = false;
+        $(this).css({
+          border: "1px solid #c0392b"
+        });
+      } else {
+        $(this).css({
+          border: "1px solid #eee"
+        });
+      }
+    });
+
+    if (!isValid) e.preventDefault();
+    if (isValid) {
+      var bmrScore = 0;
+      if (Number(formData[0].value) === 1) {
+        bmrScore = 655 + 9.6 * Number(formData[3].value) + 1.8 * Number(formData[2].value) - 4.7 * Number(formData[1].value);
+      } else {
+        bmrScore = 66 + 13.7 * Number(formData[3].value) + 5 * Number(formData[2].value) - 6.8 * Number(formData[1].value);
+      }
+      if (Number(formData[5].value) === 1) {
+        bmrScore = Math.floor(bmrScore * 1.2);
+        calculateUserBMRBasedOnType(bmrScore, formData[7].value, formData[6].value);
+      } else if (Number(formData[5].value) === 2) {
+        bmrScore = Math.floor(bmrScore * 1.375);
+        calculateUserBMRBasedOnType(bmrScore, formData[7].value, formData[6].value);
+      } else if (Number(formData[5].value) === 3) {
+        bmrScore = Math.floor(bmrScore * 1.55);
+        calculateUserBMRBasedOnType(bmrScore, formData[7].value, formData[6].value);
+      } else if (Number(formData[5].value) === 4) {
+        bmrScore = Math.floor(bmrScore * 1.55);
+        calculateUserBMRBasedOnType(bmrScore, formData[7].value, formData[6].value);
+      } else if (Number(formData[5].value) === 5) {
+        bmrScore = Math.floor(bmrScore * 1.1725);
+        calculateUserBMRBasedOnType(bmrScore, formData[7].value, formData[6].value);
+      }
+      calculateBMI((formData[3].value / formData[2].value / formData[2].value) * 10000);
+    }
+  });
+
+  function calculateBMI(bmiScore) {
+    $(".bmi-score").html(parseFloat(bmiScore).toFixed(1));
+    if (bmiScore < 16) {
+      // s$(".bmi-color").css("background-color", "#999");
+      return $(".bmi-message").html("کمبود وزن شدید");
+    } else if (bmiScore >= 16 && bmiScore < 17) {
+      // s$(".bmi-color").css("background-color", "#999");
+      return $(".bmi-message").html("کمبود وزن متوسط");
+    } else if (bmiScore >= 17 && bmiScore < 18.5) {
+      // s$(".bmi-color").css("background-color", "#999");
+      return $(".bmi-message").html("کمبود وزن ملایم");
+    } else if (bmiScore >= 18.5 && bmiScore < 25) {
+      // s$(".bmi-color").css("background-color", "#999");
+      return $(".bmi-message").html("وزن ایده ال");
+    } else if (bmiScore >= 25 && bmiScore < 30) {
+      // s$(".bmi-color").css("background-color", "#999");
+      return $(".bmi-message").html("اضافه وزن");
+    } else if (bmiScore >= 30 && bmiScore < 35) {
+      // s$(".bmi-color").css("background-color", "#999");
+      return $(".bmi-message").html("چاقی شدید درجه ۱");
+    } else if (bmiScore >= 35 && bmiScore < 40) {
+      // s$(".bmi-color").css("background-color", "#999");
+      return $(".bmi-message").html("چاقی شدید درجه ۲");
+    } else {
+      // s$(".bmi-color").css("background-color", "#999");
+      return $(".bmi-message").html("چاقی شدید درجه ۳");
+    }
+  }
+
+  function calculateUserBMRBasedOnType(calorie, duration, goal) {
+    if (Number(goal) === 2) {
+      switch (Number(duration)) {
+        case 0.2:
+          return $(".bmr-score").html(parseInt((calorie * 90) / 100));
+        case 0.3:
+          return $(".bmr-score").html(parseInt((calorie * 86.5) / 100));
+        case 0.4:
+          return $(".bmr-score").html(parseInt((calorie * 83.3) / 100));
+        case 0.5:
+          return $(".bmr-score").html(parseInt((calorie * 79) / 100));
+        case 0.6:
+          return $(".bmr-score").html(parseInt((calorie * 76) / 100));
+        case 0.7:
+          return $(".bmr-score").html(parseInt((calorie * 72.5) / 100));
+        case 0.8:
+          return $(".bmr-score").html(parseInt((calorie * 69) / 100));
+        case 0.9:
+          return $(".bmr-score").html(parseInt((calorie * 65.5) / 100));
+        case 1:
+          return $(".bmr-score").html(parseInt((calorie * 59) / 100));
+      }
+    }
+    if (Number(goal) === 3) {
+      switch (Number(duration)) {
+        case 0.2:
+          return $(".bmr-score").html(parseInt((calorie * 10) / 100 + calorie));
+        case 0.3:
+          return $(".bmr-score").html(parseInt((calorie * 13.5) / 100 + calorie));
+        case 0.4:
+          return $(".bmr-score").html(parseInt((calorie * 17) / 100 + calorie));
+        case 0.5:
+          return $(".bmr-score").html(parseInt((calorie * 21) / 100 + calorie));
+        case 0.6:
+          return $(".bmr-score").html(parseInt((calorie * 24.5) / 100 + calorie));
+        case 0.7:
+          return $(".bmr-score").html(parseInt((calorie * 28) / 100 + calorie));
+        case 0.8:
+          return $(".bmr-score").html(parseInt((calorie * 31.5) / 100 + calorie));
+        case 0.9:
+          return $(".bmr-score").html(parseInt((calorie * 35) / 100 + calorie));
+        case 1:
+          return $(".bmr-score").html(parseInt((calorie * 41) / 100 + calorie));
+      }
+    }
+    return $(".bmr-score").html(parseInt(calorie));
+  }
+
   setTimeout(function() {
     $(".loading-container").addClass("split");
   }, 1e3),
